08c7f72efb97724b3ea5e39073c3719fecca6f24
PLUSULTRA\valer <valeria03.vt@gmail.com>
2026-05-12 20:41:39 +0200

    fragment plan arreglado

--- app/src/main/java/com/example/fisioterapiaapp/fisio/FisioPlanFragment.kt
+++ app/src/main/java/com/example/fisioterapiaapp/fisio/FisioPlanFragment.kt
@@ -8,9 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.fisioterapiaapp.R
 
-class isioPlanFragment : Fragment() {
+class FisioPlanFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.activity_fisio_plan_fragment, container, false)
+        val view = inflater.inflate(R.layout.fragment_plan, container, false)
         return view
     }
 }
